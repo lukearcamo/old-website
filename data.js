20be03d24953af00c8cf2e94a3b859096ebedb34
lukea <76024089+lukearcamo@users.noreply.github.com>
2021-06-29 20:00:18 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,249 @@
+const artworks = {
+    "lAndscApes": {
+        bg: "white",
+        color: "dark",
+        path: "imgs/landscapes/",
+        description: "A lot of these works are part of my worldbuilding, hence the two-part descriptions.",
+        imgs: [
+            {
+                src: "dunes2_800na.png",
+                title: "Dunes",
+                subtitle: "Digital - March 2020",
+                description: "<i>Many alien races arrived on Kayhauhu throughout the ages. Amid the multitudes, the species who owned this particular probe has long been forgotten.</i><br><br>I took up digital art during the pandemic, and this was one of the earlier works I did. This was a reworked version of an older painting; I significantly improved the sky, and I added texture to the sand."
+            },
+            {
+                src: "treasures_900na.png",
+                title: "Treasures of Our Own",
+                subtitle: "Digital - April 2020",
+                description: "<i>While they sought the treasures of our world, we found treasures from them. It's how we've survived for so long. We scavenge, we adapt.</i><br><br>This was also a reworked version of an older painting; I made the colours stronger, I added more interesting textures and shapes, and I gave the composition more movement."
+            },
+            {
+                src: "first_contact_800na.png",
+                title: "Beyond The Hill",
+                subtitle: "Digital - June 2021",
+                description: "<i>The first aliens to arrive on Kayhauhu were humans. Increased space activity around Kayhauhu attracted the attention of others who sought the resources that the pristine planet had to offer.</i><br><br>. This was a reworked version of my very first digital landscape painting. The sky remained largely intact, I only changed the grass with my new knowledge of special brushes, and I also made the horizon and the sky features slanted to make the composition more interesting."
+            },
+            {
+                src: "third_contact_reworked_800na.png",
+                title: "Third Contact",
+                subtitle: "Digital - December 2019",
+                description: "<i>When exactly the second species arrived is unclear; a plethora of calendars and unstable celestial conditions makes long-term timekeeping unreliable.</i><br><br>This was the second digital landscape painting I ever did. I boosted the contrast to hide the mistakes in the shadows 🤡 but besides that, these clouds are one of the most unique ones I've made so far."
+            },
+            {
+                src: "fishless_reworked_800na.png",
+                title: "Moribund Livelihood",
+                subtitle: "Digital - May 2020",
+                description: "<i>The ǂiri are known for their countless failed experiments. In this abandoned laboratory, leaking fertilizer caused an algal bloom that poisoned the fish of nearby villages.</i><br><br>The three things that I was inspired to paint here were algae, water reflections, and rocks. I think the three primary colours worked really well together in this image."
+            },
+            {
+                src: "monument2_800na.png",
+                title: "The Monument that Pierced Our Souls",
+                subtitle: "Digital - May 2020",
+                description: "<i>The monument at Champs-Gyohri stands as a reminder of their failed collusion. It is said that the corrupted violet flowers grew after the battle from their spilled red and blue blood.</i><br><br>This was actually a reworked version of an older painting that I did. I modified the sky; sometimes its blank blueness is beautiful in itself."
+            },
+            {
+                src: "masked_reunion_800na.png",
+                title: "Masked Reunion",
+                subtitle: "Digital - June 2020",
+                description: "<i>After they pillaged a northern forest in search of emotion, the forest fell silent, until a ranger rediscovered the lost wildlife in new bodies.</i><br><br>This was the first time I actually painted foliage rather than using a fancy brush. Overall, a simple painting of silhouettes, but very atmospheric indeed."
+            },
+            {
+                src: "storms_reworked_800na.png",
+                title: "Blue Storms and Crimson Waves",
+                subtitle: "Digital - June 2020",
+                description: "<i>Hidden from Mah's familiar warmth, the last light seen by this coastal town was a foreign one.</i><br><br>I used a different brush for this painting, plus a lot more blending, hence it looks very soft and blurry. I honestly don't know if that's a good thing."
+            },
+        ]
+    },
+    "quiet probleMs": {
+        bg: "black",
+        color: "white",
+        description: "A series of artworks intended to raise awareness about global issues that are often overlooked.",
+        path: "imgs/quiet_problems/",
+        imgs: [
+            {
+                src: "lang_death_800na.png",
+                title: "Speak With A Coloured Tongue",
+                subtitle: "Digital - April 2021",
+                description: "This painting illustrates the ongoing resistance against language loss. The attempt to silence a language is thwarted by the act of removing the white tape, symbolizing defiance against assimilation."
+            },
+            {
+                src: "urbanization_postproc_800na.png",
+                title: "False Opportunities",
+                subtitle: "Digital - May 2021",
+                description: "This painting illustrates the phenomenon of urbanization. Despite cities providing better opportunities at life for many people, they also present a different set of challenges, such as overpopulation, pollution, and loss of culture."
+            },
+            {
+                src: "assimilation_800na.png",
+                title: "Ancestors Calling",
+                subtitle: "Digital - May 2021",
+                description: "This painting illustrates the process of cultural assimilation. As people transform themselves in order to integrate into contemporary society, it is important to never forget one's culture, traditions, and ancestry. Otherwise, a part of one's identity dies, forgotten by all but one's ancestors, lost in the depths of history."
+            }
+        ]
+    },
+    "portrAits": {
+        bg: "dark",
+        color: "white",
+        path: "imgs/portraits/",
+        imgs: [
+            {
+                src: "portrait_800na.png",
+                title: "Self-Portrait",
+                subtitle: "Digital - April 2021",
+                description: "\"I am Narcissus and my little zoom square is my lake\"<br>— @somfeelingood on twitter<br><br>During one of my online classes, the natural lighting from my window was stunning so I painted my little thumbnail. I was particularly captivated by how my webcam omits details in extreme light and shadow. This is also the portrait I use on my about page."
+            },
+            {
+                src: "airpods_800na.png",
+                title: "Meisje Met De Airpods",
+                subtitle: "Digital - January 2021",
+                description: "This was a recreation of the famous painting by Johannes Vermeer, my favourite painting of all time. I added airpods to make it creative and modern, but then <i>Google Images</i> shatters my dreams and shows me existing memes of this same idea. Nevertheless, this was my first digital portrait painting, which inspired me to paint more portraits."
+            },
+            {
+                src: "hair_reworked_800na.png",
+                title: "The Gifts of Photosynthesis and Hair",
+                subtitle: "Digital - May 2021",
+                description: "<i>Centuries of genetic mixing and experimentation has altered the flora and fauna of Kayhauhu. This particular girl can photosynthesize like most native creatures, yet she is part of the Yanwa population's 20% who possess hair like humans. Horizontal pupils are a vestigial evolutionary trait from the Yanwa who were hunted.</i><br><br>I wasn't particularly happy about this painting, something just feels off about it, whether it's the facial proportions or the shading. I managed to fix it a bit, because the original was a lot worse."
+            },
+            {
+                src: "hunter_800na.png",
+                title: "A Young Blö Hunter",
+                subtitle: "Digital - January 2021",
+                description: "<i>It isn't clear what the Blö of Kumunuo Forest are; only hunters like this boy have seen their visible form using traditional drugs and cybernetics.</i><br><br>This was a character for a short story that currently only exists in my mind which I have yet to write. In this painting, I particularly like the fur coat, and the face paint pattern."
+            },
+            {
+                src: "council_800na.png",
+                title: "The Council",
+                subtitle: "Digital - February 2021",
+                description: "I made this painting simply because I wanted to see the three most iconic internet dogs together: Cheems, Gabe, and Doge."
+            }
+        ]
+    },
+    "birds & fruits": {
+        bg: "light",
+        color: "white",
+        path: "imgs/birds/",
+        description: "no thoughts, head empty, only bird",
+        imgs: [
+            {
+                src: "kiwi_800na.png",
+                title: "Kiwi & kiwi",
+                subtitle: "Digital - September 2020",
+                description: ""
+            },
+            {
+                src: "pitaya_800na.png",
+                title: "Pitaya & Flamingo",
+                subtitle: "Digital - September 2020",
+                description: ""
+            },
+            {
+                src: "orange_800na.png",
+                title: "Orange & Hummingbird",
+                subtitle: "Digital - September 2020",
+                description: ""
+            }
+        ]
+    },
+    "studies & rAndoM stuff": {
+        bg: "white",
+        color: "dark",
+        path: "imgs/general/",
+        imgs: [
+            {
+                src: "nomatter_800na.png",
+                title: "No Matter What Happens",
+                subtitle: "Digital - May 2021",
+                description: "<i>Many cities in Kayhauhu are inhabited by a mix of native, alien, and hybrid races. Sociopolitical division between races is prevalent, and danger befalls those who are not cautious.</i><br><br>This was a painting I made for Mother's Day, depicting a mother's unwaivering and protective love for her children."
+            },
+            {
+                src: "adapt_reworked_800na.png",
+                title: "Adapt",
+                subtitle: "Digital - August 2020",
+                description: "<i>For the natives of Kayhauhu, adaptation is necessary for survival. As a child, ʻNayé's parents died in a ǂiri raid. She had to give up her nomadic lifestyle and move to the city where she now sells scavenged electronics in her small shop.</i><br><br>This was the first time I ever used image textures on a painting. I think it added more detail which is nice, but it can be difficult finding the perfect texture for an object."
+            },
+            {
+                src: "gender_reveal_800na.png",
+                title: "Was it Worth It?",
+                subtitle: "Digital - March 2021",
+                description: "This was supposed to be part of the Quiet Problems painting series above, but it didn't really fit so I separated it. This painting was inspired by the 2020 California wildfire caused by a gender reveal party. This was by far the fastest painting I've ever made, finishing in under 2 hours."
+            },
+            {
+                src: "noodles_postproc_800na.png",
+                title: "Noodles",
+                subtitle: "Digital - July 2020",
+                description: "I had ramen at a friend's house and it looked nice so I painted it. I tried making it look \"wet\" using highlights but I didn't have a good enough understanding of light to make it realistic or accurate."
+            },
+            {
+                src: "shore_800na.png",
+                title: "Where It Washed Ashore",
+                subtitle: "Digital - July 2020",
+                description: "<i>Two days after Kahauafanurua went missing, he washed back ashore, unconscious and without memories. The next day, another arrived. As we later learned, one of them was not real; a mimic sent to steal the object that had protected our island for years.</i><br><br>For this piece, there was no inspiration other than a versatile bubbly watercolor brush that I found. Since then, I have used it for waves, foliage, and food."
+            },
+            {
+                src: "mountain_study_800na.png",
+                title: "Untitled",
+                subtitle: "Digital - October 2020",
+                description: "This study was based on an older mountain acrylic painting I made a few years ago. Here, I used a more hard-edged untextured brush with very little blending, and I think it worked really well to convey the mountain's roughness."
+            },
+            {
+                src: "canal_800na.png",
+                title: "Untitled",
+                subtitle: "Digital - June 2021",
+                description: "Yet another study to practice foliage and water, without using special brushes or hiding details in dark silhouettes. The technique I found when painting foliage was to keep adding finer and finer leaf details. I think it could be improved by spending more time adding details and reducing the amount of blending."
+            },
+            {
+                src: "train_900na.png",
+                title: "Untitled",
+                subtitle: "Digital - July 2020",
+                description: "For this study, I wanted to try painting fields of grass, as well as green rounded hills, since most of the mountains I've painted are blue, distant, and rocky. I didn't think the composition was balanced so I added a person, but I don't know if that helped."
+            },
+            // {
+            //     src: "alley_800na.png",
+            //     title: "Untitled",
+            //     subtitle: "Digital - June 2021",
+            //     description: "This was yet another grass study, but I prefer this grass style over the previous one. This was supposed to have a worldbuilding backstory to it, but this particular painting of mine is probably one of my least favorites."
+            // },
+            {
+                src: "waystone_postproc_800na.png",
+                title: "Waystone",
+                subtitle: "Digital - May 2020",
+                description: "Another foliage study except I used special brushes :P. Not happy with the symmetrical composition and the overall darkness/solidness of the colour, but I do like the limited blending."
+            },
+            // {
+            //     src: "rocks_800na.png",
+            //     title: "Rock Study",
+            //     subtitle: "Digital - February 2020",
+            //     description: "Just some rocks I found on google images and also the ground. I struggle with painting rocks because I make them too precise and jagged, so I should probably paint them with a more relaxed and painterly style."
+            // },
+        ]
+    }
+};
+
+const code = {
+    "some projects": {
+        bg: "black",
+        color: "white",
+        description: "Blah Blah Blah.",
+        path: "imgs/code/",
+        imgs: [
+            {
+                src: "random_image.png",
+                title: "Random Thing",
+                demo: "https://lukearcamo.github.io/The_Backrooms/",
+                github: "https://github.com/lukearcamo/The_Backrooms/",
+                subtitle: "JS (WebGL with THREE.js)",
+                description: "This is a description of the piece of code."
+            },
+            {
+                src: "abc.png",
+                title: "A Thing",
+                demo: "https://github.com/lukearcamo",
+                github: "https://github.com/lukearcamo",
+                subtitle: "C++ (OpenGL)",
+                description: "This is a description of the piece of code."
+            }
+        ]
+    }
+};
+
+export { artworks, code };
